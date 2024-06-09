00accd724760cd0037b5e14250e44c1b1f84705f
Levente <145211298+fulopl@users.noreply.github.com>
2024-06-09 17:03:29 +0200

    add maven starter code

---
+++ src/main/java/com/codecool/solarwatchmvpjavafulopl/SolarWatchMvpJavaFuloplApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.solarwatchmvpjavafulopl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarWatchMvpJavaFuloplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarWatchMvpJavaFuloplApplication.class, args);
+	}
+
+}
