05f79872f209ce43c5a061a271df6e6dfc317e8a
Levente <145211298+fulopl@users.noreply.github.com>
2024-07-30 10:44:10 +0200

    add UserService and UserRepository

---
+++ src/main/java/com/codecool/solarwatch/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.codecool.solarwatch.repository;
+
+import com.codecool.solarwatch.model.entity.Role;
+import com.codecool.solarwatch.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserEntityByUsername(String username);
+
+    Optional<UserEntity> updateUserEntity(UserEntity user);
+
+}
