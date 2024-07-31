247391a05541a26cc2a46b6d96a7317fb61b51fa
Levente <145211298+fulopl@users.noreply.github.com>
2024-07-31 15:37:46 +0200

    fix registration page

--- client/src/Pages/RegistrationPage.jsx
+++ client/src/Pages/RegistrationPage.jsx
@@ -1,6 +1,12 @@
 import UserForm from "../Components/UserForm";
+import {useNavigate} from "react-router-dom";
 
 const RegistrationPage = () => {
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        return 0
+    }
 
     return (
         <UserForm
