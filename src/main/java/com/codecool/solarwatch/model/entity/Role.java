6ea4f5c7819e0c54c9a43cc850f7f72602aceb3f
Levente <145211298+fulopl@users.noreply.github.com>
2024-07-31 13:20:48 +0200

    add UserController

--- src/main/java/com/codecool/solarwatch/model/entity/Role.java
+++ src/main/java/com/codecool/solarwatch/model/entity/Role.java
@@ -3,6 +3,9 @@ package com.codecool.solarwatch.model.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
 
 @Entity
 public class Role {
@@ -11,6 +14,15 @@ public class Role {
     @GeneratedValue
     private Long id;
     private String name;
+    @ManyToMany(mappedBy = "roles")
+    private List<UserEntity> users;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role() {
+    }
 
     public void setId(Long id) {
         this.id = id;
