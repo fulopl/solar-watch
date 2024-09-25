48b5a7b29a507efa975d3d8fdbcfc8f37fb4bf76
Levente <145211298+fulopl@users.noreply.github.com>
2024-09-25 12:19:09 +0200

    change exception type (NoSuchElement) and add to Advisor class

--- src/main/java/com/codecool/solarwatch/controller/ControllerAdvice.java
+++ src/main/java/com/codecool/solarwatch/controller/ControllerAdvice.java
@@ -1,13 +1,21 @@
 package com.codecool.solarwatch.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.View;
 
-@ControllerAdvice
-public class SolarWatchControllerAdvice {
+import java.util.NoSuchElementException;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+
+    private final View error;
+
+    public ControllerAdvice(View error) {
+        this.error = error;
+    }
 
     @ResponseBody
     @ExceptionHandler(InvalidLocationException.class)
@@ -18,8 +26,15 @@ public class SolarWatchControllerAdvice {
 
     @ResponseBody
     @ExceptionHandler(ThirdPartyServiceException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String thirdPartyServiceHandler(ThirdPartyServiceException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String noSuchElementHandler(NoSuchElementException ex) {
         return ex.getMessage();
     }
 }
--- src/main/java/com/codecool/solarwatch/service/UserService.java
+++ src/main/java/com/codecool/solarwatch/service/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import static java.lang.String.format;
@@ -34,16 +35,16 @@ public class UserService {
 
         String username = contextUser.getUsername();
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new IllegalArgumentException(format("could not find user %s in the repository", username)));
+                .orElseThrow(() -> new NoSuchElementException("No such user."));
 
     }
 
     public void addRoleFor(Long userId, String roleName) {
         UserEntity user = userRepository.findById(userId).orElse(null);
-        if (user == null) throw new IllegalArgumentException("No such user."); // TODO ex.handling
+        if (user == null) throw new NoSuchElementException("No such user.");
 
         Role role = roleRepository.findByName(roleName);
-        if (role == null) throw new IllegalArgumentException("No such role."); // TODO ex.handling
+        if (role == null) throw new NoSuchElementException("No such role.");
 
         user.getRoles().add(role);
         userRepository.save(user);
@@ -51,18 +52,18 @@ public class UserService {
 
     public void removeRoleFrom(Long userId, String roleName) {
         UserEntity user = userRepository.findById(userId).orElse(null);
-        if (user == null) throw new IllegalArgumentException("No such user."); // TODO ex.handling
+        if (user == null) throw new NoSuchElementException("No such user.");
 
         Role role = roleRepository.findByName(roleName);
-        if (role == null) throw new IllegalArgumentException("No such role."); // TODO ex.handling
+        if (role == null) throw new NoSuchElementException("No such role.");
 
-        user.getRoles().remove(role);
+        if (!user.getRoles().remove(role)) throw new NoSuchElementException("No such role with user.");
         userRepository.save(user);
     }
 
     public void deleteUser(Long id) {
         if (userRepository.existsById(id)) userRepository.deleteById(id);
-        else throw new IllegalArgumentException("No such user."); // TODO ex.handling
+        else throw new NoSuchElementException("No such user.");
     }
 
     public List<UserResponse> getAllUsers() {
