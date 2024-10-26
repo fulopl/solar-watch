bc7b2362575033adf32d800a1fe39ab66a5823f4
Levente <145211298+fulopl@users.noreply.github.com>
2024-10-26 21:35:19 +0200

    make auth entry point send custom error messages to frontend

--- src/main/java/com/codecool/solarwatch/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/codecool/solarwatch/security/jwt/AuthEntryPointJwt.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,11 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response
             , AuthenticationException authException) throws IOException, ServletException {
-        logger.error("Security error: {}", authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+        String errorMsg = authException.getMessage();
+        logger.error("Security error: {}", errorMsg);
+        //response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write("{\"error\": \"" + errorMsg + "\"}");
     }
 }
