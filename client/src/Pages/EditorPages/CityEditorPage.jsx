5baebc56d55539733021ac65a6151542d2c7f358
Levente <145211298+fulopl@users.noreply.github.com>
2024-11-05 22:18:33 +0100

    remove console.log from CityEd

--- client/src/Pages/EditorPages/CityEditorPage.jsx
+++ client/src/Pages/EditorPages/CityEditorPage.jsx
@@ -25,14 +25,13 @@ const deleteCity = (id) => {
                 }
         }
     ).then(resp => {
-        console.log(resp)
         if (resp.ok) return "OK"
         else return resp.text()
             .then(text => text)
             .catch((error) => {
                 return resp.statusText
             })
-    }).catch(error => error)
+    }).catch(error => error);
 }
 
 const CityEditorPage = () => {
@@ -46,14 +45,14 @@ const CityEditorPage = () => {
             setLoading(false);
             setCities(cities);
         })
-    }, [])
+    }, [serverMsg])
 
     const handleDelete = (id) => {
         setLoading(true);
         deleteCity(id).then((response) => {
             setLoading(false);
             if (response === "OK") setCities(cities.filter(city => city.id !== id))
-            else setServerMsg(response)
+            else setServerMsg(response);
         });
     }
 
